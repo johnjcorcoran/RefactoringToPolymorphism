e50dfcd7f897c1ad07aa2169b7bf02cd8010ebbc
Leon Hewitt <leon.hewitt@7digital.com>
2014-09-09 17:36:48 +0100

    methods added to bottles to make build pass

--- NinetyNineBottlesOfBeer/Bottles.cs
+++ NinetyNineBottlesOfBeer/Bottles.cs
@@ -2,5 +2,14 @@
 {
     public class Bottles
     {
+        public string Verse(int num)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Verse(int lowerBound, int upperBound)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
