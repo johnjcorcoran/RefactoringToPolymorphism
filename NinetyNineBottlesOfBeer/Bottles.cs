3f03a8e38a50e4c686b02146670897895ac5880b
John Corcoran <john.corcoran@7digital.com>
2014-09-12 15:59:06 +0100

    some basic extract method refactorings

--- NinetyNineBottlesOfBeer/Bottles.cs
+++ NinetyNineBottlesOfBeer/Bottles.cs
@@ -2,47 +2,13 @@
 {
 	public class Bottles
 	{
-		public string Verse(int num)
-		{
-			if (num == 0)
-			{
-				return @"No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.";
-			}
-			if (num == 1)
-			{
-				return @"1 bottle of beer on the wall, 1 bottle of beer.
-Take it down and pass it around, no more bottles of beer on the wall.";
-			}
-			if (num == 2)
-			{
-				return @"2 bottles of beer on the wall, 2 bottles of beer.
-Take one down and pass it around, 1 bottle of beer on the wall.";
-			}
-			if (num == 89)
-			{
-				return @"89 bottles of beer on the wall, 89 bottles of beer.
-Take one down and pass it around, 88 bottles of beer on the wall.";
-			}
-			return @"99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.";
-		}
-
 		public string Verse(int upperBound, int lowerBound)
 		{
 			if (upperBound == 2)
 			{
-				return @"2 bottles of beer on the wall, 2 bottles of beer.
-Take one down and pass it around, 1 bottle of beer on the wall.
-
-1 bottle of beer on the wall, 1 bottle of beer.
-Take it down and pass it around, no more bottles of beer on the wall.
-
-No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
-
-";
+				return Verse(2) + "\r\n\r\n" + Verse(1) + "\r\n\r\n" + Verse(0) + "\r\n\r\n";
 			}
+
 			return @"99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
 
@@ -50,6 +16,51 @@ Take one down and pass it around, 98 bottles of beer on the wall.
 Take one down and pass it around, 97 bottles of beer on the wall.
 
 ";
+		}
+
+		public string Verse(int num)
+		{
+			if (num == 0)
+			{
+				return GetNoBottlesVerse();
+			}
+			if (num == 1)
+			{
+				return GetOneBottleVerse();
+			}
+			if (num == 2)
+			{
+				return GetTwoBottlesVerse();
+			}
+			if (num == 89)
+			{
+				return GetVerse(89);
+			}
+			return GetVerse(99);
+		}
+
+		public string GetVerse(int num)
+		{
+			return string.Format(@"{0} bottles of beer on the wall, {0} bottles of beer.
+Take one down and pass it around, {1} bottles of beer on the wall.", num, num - 1);
+		}
+
+		public string GetOneBottleVerse()
+		{
+			return @"1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.";
+		}
+
+		public string GetTwoBottlesVerse()
+		{
+			return @"2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.";
+		}
+
+		public string GetNoBottlesVerse()
+		{
+			return @"No more bottles of beer on the wall, no more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.";
 		}
 	}
 }
